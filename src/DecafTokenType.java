53d664826d4edb4854ce4f4e62d80fe6001726bb
Christian Bargraser <39784989+CoderTypist@users.noreply.github.com>
2022-02-26 04:57:36 -0600

    Fixed spacing

--- src/DecafTokenType.java
+++ src/DecafTokenType.java
@@ -1,68 +1,68 @@
 
 public enum DecafTokenType implements TokenType {
-    // - identifier -
-    T_XXXX { return "T_XXXX"; } },
+   	// - identifier -
+   	T_XXXX { return "T_XXXX"; } },
 	
-    // - data types -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+   	// - data types -
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
 	
-    // - data type constants -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+   	// - data type constants -
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
 
-    // - flow control -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+   	// - flow control -
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
 
-    // - special data types -
-    T_XXXX { return "T_XXXX"; } },
+   	// - special data types -
+   	T_XXXX { return "T_XXXX"; } },
 
-    // - special values -
-    T_XXXX { return "T_XXXX"; } },
+   	// - special values -
+   	T_XXXX { return "T_XXXX"; } },
 	
-    // - comparisons -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } }, 
+   	// - comparisons -
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } },
+   	T_XXXX { return "T_XXXX"; } }, 
 	
-    // - logical operators -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+	// - logical operators -
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
     
-    // - arithmetic -
-    T_XXXX { return "T_XXXX"; } },
-    
-    // - special characters -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+	// - arithmetic -
+	T_XXXX { return "T_XXXX"; } },
+
+	// - special characters -
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
 	
-    // - i/o -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+	// - i/o -
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
 	
-    // - comments -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+	// - comments -
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
 	
-    // - errors -
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
-    T_XXXX { return "T_XXXX"; } },
+	// - errors -
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
+	T_XXXX { return "T_XXXX"; } },
 	
-    // - invalid token -
-    T_XXXX { return "T_XXXX"; } }
+	// - invalid token -
+	T_XXXX { return "T_XXXX"; } }
 }
